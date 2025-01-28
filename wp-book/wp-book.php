c342ef3ae9c11ddd63b064cd4a1eec4d9b1a1ff5
Eleas Kanchon <eleas@softzino.com>
2025-01-28 22:56:50 +0600

    Internationalize: Internationalize the plugin

--- wp-book/wp-book.php
+++ wp-book/wp-book.php
@@ -5,6 +5,7 @@
  * Version: 1.0.0
  * Author: Eleas Kanchon
  * Text Domain: wp-book
+ * Domain Path: /languages
  */
 
  if ( ! defined( 'ABSPATH' ) ) {
@@ -18,6 +19,11 @@ define( 'WP_BOOK_URL', plugin_dir_url( __FILE__ ) );
 
 // Load necessary files.
 require_once WP_BOOK_PATH . 'includes/class-loader.php';
+
+add_action( 'plugins_loaded', function () {
+    load_plugin_textdomain( 'wp-book', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+} );
+
 
 // Register activation and deactivation hooks.
 register_activation_hook( __FILE__, [ 'WPBook\Includes\Activator', 'activate' ] );
